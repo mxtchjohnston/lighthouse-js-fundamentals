5a63c9460004e17c3384aa03bfad469cacab8f40
mxtchjohnston <mitchjohnston99@gmail.com>
2023-08-21 22:27:27 +0000

    nothing interestin

--- tinkering.js
+++ tinkering.js
@@ -2,7 +2,15 @@ function multiply(a, b) {
     return a * b;
   }
   
-  const result = multiply(2, 4);
+const result = multiply(2, 4);
   
-  console.log(result);
-  
+console.log(result);
+
+var donuts = ["glazed", "chocolate frosted", "boston cream", "powdered", "sprinkled", "maple", "coconut", "jelly"];
+
+
+donuts[2] = "cinnamon twist";
+donuts[4] = "salted caramel";
+donuts[5] = "shortcake eclair";
+
+console.log(donuts)

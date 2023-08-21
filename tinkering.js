d2b5977a5e14c324c844bf224ba08421dcfd68d8
mxtchjohnston <mitchjohnston99@gmail.com>
2023-08-21 22:50:25 +0000

    range function

--- tinkering.js
+++ tinkering.js
@@ -1,16 +1,32 @@
-function multiply(a, b) {
-    return a * b;
+// function multiply(a, b) {
+//     return a * b;
+//   }
+  
+// const result = multiply(2, 4);
+  
+// console.log(result);
+
+// var donuts = ["glazed", "chocolate frosted", "boston cream", "powdered", "sprinkled", "maple", "coconut", "jelly"];
+
+
+// donuts[2] = "cinnamon twist";
+// donuts[4] = "salted caramel";
+// donuts[5] = "shortcake eclair";
+
+// console.log(donuts)
+
+const range = (start, end, step) => {
+  let array = []
+  if (start === undefined || end === undefined || step === undefined || start > end || step >= 0){
+    return array;
   }
-  
-const result = multiply(2, 4);
-  
-console.log(result);
 
-var donuts = ["glazed", "chocolate frosted", "boston cream", "powdered", "sprinkled", "maple", "coconut", "jelly"];
+  for (let i = start; i < end; i = i + step){
+    array.push(i);
+  }
+  return array;
+}
 
-
-donuts[2] = "cinnamon twist";
-donuts[4] = "salted caramel";
-donuts[5] = "shortcake eclair";
-
-console.log(donuts)
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));

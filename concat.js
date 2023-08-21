3ab6a8b8e4960a8ce49e9a6e519eae8a9f54cf54
mxtchjohnston <mitchjohnston99@gmail.com>
2023-08-21 23:53:13 +0000

    concat

---
+++ concat.js
@@ -0,0 +1,12 @@
+const concat = function(arr1, arr2){
+  for (let elem of arr2) {
+    //console.log(elem);
+    arr1.push(elem);
+  }
+  return arr1
+}
+
+console.log(concat([1, 2, 3], [4, 5, 6]), "=?", [1, 2, 3, 4, 5, 6]);
+console.log(concat([0, 3, 1], [9, 7, 2]), "=?", [0, 3, 1, 9, 7, 2]);
+console.log(concat([], [9, 7, 2]), "=?", [9, 7, 2]);
+console.log(concat([5, 10], []), "=?", [5, 10]);

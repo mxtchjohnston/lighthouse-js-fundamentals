6604d10d138466d86fee858902b52aefbed1b46a
Mitchell Stave <mitchjohnston99@gmail.com>
2023-09-02 16:32:58 -0700

    Kata 2

--- Kata/Kata2.js
+++ Kata/Kata2.js
@@ -1,7 +1,22 @@
+//Added constraint supressing use of filter
+// values . filter . fold (+)
+
 const conditionalSum = function(values, condition) {
-    // Your code here
+    let sum = 0; //initialized running sum 
+    values.forEach(element => {
+      if (condition === "odd" && isOdd(element)){
+        sum += element;
+      } else if (condition === "even" && !isOdd(element)){
+        sum += element;
+      }
+    }); //elegance
+    return sum; // guaranteed return of zero.
   };
-  
+
+const isOdd = function(i){
+  return (i % 2 === 1);
+}
+
 console.log(conditionalSum([1, 2, 3, 4, 5], "odd"));
 console.log(conditionalSum([13, 88, 12, 44, 99], "even"));
 console.log(conditionalSum([1, 2, 3, 4, 5], "even"));
